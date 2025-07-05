21fb39e872751046200fba94d02b5b3499cdc4af
JackMills <JackMills@NotRealEmail.com>
2025-07-05 12:03:02 +0100

    missing file

---
+++ vectors.h
@@ -0,0 +1,61 @@
+#pragma once
+
+#include "types.h"
+
+template <size_t Dim>
+int64_t Dot (Vector<Dim> const & a, Vector<Dim> const & b)
+{
+    int64_t ret = 0;
+    for (size_t i = 0; i < Dim; i++)
+    {
+        ret += a.mValues[i] * b.mValues[i];
+    }
+
+    return ret;
+}
+
+template <size_t Dim>
+double DotSafe (Vector<Dim> const & a, Vector<Dim> const & b)
+{
+    double ret = 0;
+    for (size_t i = 0; i < Dim; i++)
+    {
+        ret += static_cast<double>(a.mValues[i]) * b.mValues[i];
+    }
+
+    return ret;
+}
+
+template <size_t Dim>
+Vector<Dim> Diff(Vector<Dim> const & a, Vector<Dim> const & b)
+{
+    Vector<Dim> result;
+
+    for (size_t i = 0; i < Dim; i++)
+    {
+        result.mValues[i] = a.mValues[i] - b.mValues[i];
+    }
+
+    return result;
+}
+
+template <size_t Dim>
+void Normalize(Vector<Dim> & point)
+{
+    auto squareMag = Dot(point, point);
+    auto divisor = std::sqrt(squareMag);
+    for (auto & coord : point.mValues)
+    {
+        // Round up I guess? Todo think more
+        coord = ((coord * ScaledOne) + divisor - 1)/ divisor;
+    }
+}
+
+template <size_t Dim>
+void Normalize(std::vector<Vector<Dim>> & points)
+{
+    for (auto & point : points)
+    {
+        Normalize(point);
+    }
+}
